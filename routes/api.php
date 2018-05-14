86fba49528f2448406a17beced24a7f170f76d06
Jaaouani Oussama <jaaouani@Jaaouanis-MacBook-Air.local>
2018-05-14 02:32:50 +0200

    adding mailgun

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Http\Request;
 
-Route::group(['prefix' => 'api/v1/account'], function(Request $request) {
+Route::group(['prefix' => 'api/v1/account'], function() {
         Route::middleware('guest:api')->post('login', 'v1\API\AccountController@login');
         Route::middleware('guest:api')->post('register', 'v1\API\AccountController@register');
         Route::middleware('guest:api')->post('forgot', 'v1\API\AccountController@forgot');
         Route::middleware('auth:api')->post('logout', 'v1\API\AccountController@logout');
 });
-Route::group(['prefix' => 'api/v1/profile'], function(Request $request) {
+Route::group(['prefix' => 'api/v1/profile'], function() {
         Route::middleware('auth:api')->get('retrieve', 'v1\API\ProfileController@retrieve');
-        Route::post('auth:api')->post('update/fullname', 'v1\API\ProfileController@fullname');
-        Route::post('auth:api')->post('update/password', 'v1\API\ProfileController@email');
-        Route::post('auth:api')->post('update/email', 'v1\API\ProfileController@password');
+        Route::middleware('auth:api')->post('update/fullname', 'v1\API\ProfileController@fullname');
+        Route::middleware('auth:api')->post('update/password', 'v1\API\ProfileController@email');
+        Route::middleware('auth:api')->post('update/email', 'v1\API\ProfileController@password');
 });
